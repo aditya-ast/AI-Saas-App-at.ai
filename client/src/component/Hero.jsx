b34f26c552812119afdfdb16d02e164cc355ffa4
aditya-ast <adisingh7478@gmail.com>
2025-08-28 02:20:20 +0530

    Adding Authentication and Authrization by gmail with ( clerk.com )

---
+++ client/src/component/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { assets } from "../assets/assets";
+
+function Hero() {
+  return (
+    <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(./gradientBackground.png)] bg-cover bg-no-repeat'>
+<h1>hellloooooooo</h1>
+    </div>
+  )
+}
+
+export default Hero
--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { ArrowRight } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 function Navbar() {
+  const navigator = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
+      <img
+        src={assets.logo}
+        alt="Logo"
+        className="w-32 sm:w-44 cursor-pointer"
+        onClick={() => navigator("/")}
+      />
+
+      {user ? (
+        <div className="scale-150 my-3">
+          <UserButton />
+        </div>
+      ) : (
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-black text-white py-2.5 px-10"
+        >
+          Get Started <ArrowRight className="w-5 h-5" />
+        </button>
+      )}
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
